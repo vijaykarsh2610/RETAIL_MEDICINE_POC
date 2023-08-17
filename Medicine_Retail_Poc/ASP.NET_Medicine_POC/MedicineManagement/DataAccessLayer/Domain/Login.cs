19867235450edb459dfa35b5f792a0948c64327f
katyayanisahu <88482844+katyayanisahu@users.noreply.github.com>
2023-08-17 12:00:35 +0530

    Modified Domain Classes

--- Medicine_Retail_Poc/ASP.NET_Medicine_POC/MedicineManagement/DataAccessLayer/Domain/Login.cs
+++ Medicine_Retail_Poc/ASP.NET_Medicine_POC/MedicineManagement/DataAccessLayer/Domain/Login.cs
@@ -14,11 +14,11 @@ namespace DataAccessLayer.Domain
 
         [Required(ErrorMessage = "Email is required.")]
         [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email format.")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "Please enter your Password.")]
         [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
-        public string Password { get; set; }
+        public string? Password { get; set; }
 
         [Required(ErrorMessage = "IsAdmin is required.")]
         public bool IsAdmin { get; set; }
